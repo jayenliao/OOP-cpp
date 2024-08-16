9bb6000d0c59ab36acbfd3007bab379af03e60e3
jayenliao <jay.liao@fazzfinancial.com>
2024-08-16 14:57:46 +0800

    feat: implement CSVReader

--- C3M2/CSVReader.cpp
+++ C3M2/CSVReader.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include "CSVReader.h"
 
 using namespace std;
@@ -15,13 +16,24 @@ vector<string> CSVReader::tokenize(string csvLine, char separator) {
     vector<string> tokens;
     return tokens;
 }
-OrderBookEntry CSVReader::strToOBE(vector<string> strings) {
+OrderBookEntry CSVReader::strToOBE(vector<string> tokens) {
+    if (tokens.size() != 5) {
+        cout << "\nError: a bad line!\n" << endl;
+        throw exception{};
+    }
+    double price, amount;
+    try {
+        price = stod(tokens[3]);
+        amount = stod(tokens[4]);
+    } catch (exception& e) {
+        cout << "Bad float(s)! " << tokens[3] << " | " << tokens[4] << endl;
+    }
     OrderBookEntry obe{
-        "_timestamp",
-        "_product",
-        OrderBookType::bid,
-        1,
-        1
+        tokens[0],
+        tokens[1],
+        OrderBookEntry::strToOBT(tokens[2]),
+        price,
+        amount
     };
     return obe;
 }

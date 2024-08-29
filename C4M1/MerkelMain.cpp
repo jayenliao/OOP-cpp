7906dca35ecd4901433a8f9345b8ef03ab94ad31
jayenliao <jay.liao@fazzfinancial.com>
2024-08-29 17:16:01 +0800

    feat: change printMarketStats()

--- C4M1/MerkelMain.cpp
+++ C4M1/MerkelMain.cpp
@@ -39,15 +39,18 @@ void MerkelMain::printHelp() {
 }
 
 void MerkelMain::printMarketStats() {
-    cout << "OrderBook contains: " << orders.size() << " entries.\n" << endl;
-    unsigned int bids = 0;
-    unsigned int asks = 0;
-    for (OrderBookEntry& entry : orders) {
-        if (entry.orderType == OrderBookType::bid) {++bids;}
-        if (entry.orderType == OrderBookType::ask) {++asks;}
+    for (string const& p : orderBook.getKnownProducts()) {
+        cout << "Product: " << p << endl;
     }
-    cout << "Number of bids = " << bids << endl;
-    cout << "Number of asks = " << asks << endl;
+    // cout << "OrderBook contains: " << orders.size() << " entries.\n" << endl;
+    // unsigned int bids = 0;
+    // unsigned int asks = 0;
+    // for (OrderBookEntry& entry : orders) {
+    //     if (entry.orderType == OrderBookType::bid) {++bids;}
+    //     if (entry.orderType == OrderBookType::ask) {++asks;}
+    // }
+    // cout << "Number of bids = " << bids << endl;
+    // cout << "Number of asks = " << asks << endl;
 }
 
 void MerkelMain::enterOffer() {

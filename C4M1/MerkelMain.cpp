1eed2f25ae07855ad34873b1ff22005b064105c7
jayenliao <jay.liao@fazzfinancial.com>
2024-08-29 16:48:20 +0800

    feat: add existing files from previous courses

---
+++ C4M1/MerkelMain.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include "MerkelMain.h"
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+
+using namespace std;
+
+MerkelMain::MerkelMain() {
+
+}
+
+void MerkelMain::init() {
+    loadOrderBook();
+    int input;
+    while (true) {
+        printMenu();
+        input = getUserOption();
+        processUserOption(input);
+    }
+}
+
+void MerkelMain::loadOrderBook() {
+    orders = CSVReader::readCSV("/Users/jayliao/Documents/2_areas/001_programming/cpp/coursera_OOP_specialization/C3M1/Merkelerex/src/20200317.csv");
+}
+
+void MerkelMain::printMenu() {
+    cout << "----- MerkleRex Menu -----" << endl;
+    cout << "1. Print help" << endl;
+    cout << "2. Print exchange stats" << endl;
+    cout << "3. Make an ask" << endl;
+    cout << "4. Make a bid" << endl;
+    cout << "5. Print wallet" << endl;
+    cout << "6. Continue" << endl;
+}
+
+void MerkelMain::printHelp() {
+    cout << "Help: Your aim is to make money, analyze the market, and make bids and offers.\n" << endl;
+}
+
+void MerkelMain::printMarketStats() {
+    cout << "OrderBook contains: " << orders.size() << " entries.\n" << endl;
+    unsigned int bids = 0;
+    unsigned int asks = 0;
+    for (OrderBookEntry& entry : orders) {
+        if (entry.orderType == OrderBookType::bid) {++bids;}
+        if (entry.orderType == OrderBookType::ask) {++asks;}
+    }
+    cout << "Number of bids = " << bids << endl;
+    cout << "Number of asks = " << asks << endl;
+}
+
+void MerkelMain::enterOffer() {
+    cout << "Make an offer: Enter the amount to ask.\n" << endl;
+}
+
+void MerkelMain::enterBid() {
+    cout << "Make a bid: Enter the amount to bid.\n" << endl;
+}
+
+void MerkelMain::printWallet() {
+    cout << "Print wallet: Your wallet is empty.\n" << endl;
+}
+
+void MerkelMain::gotoNextTimeFrame() {
+    cout << "Continue: Going to next time frame.\n" << endl;
+}
+
+void MerkelMain::warnInvalid() {
+    cout << "Invalid choice. Please select an integer between 1 and 6.\n" << endl;
+}
+
+int MerkelMain::getUserOption() {
+    int userOption;
+    cout << "Select an option from 1 to 6: ";
+    cin >> userOption;
+    cout << "Your choice: " << userOption << endl;
+    return userOption;
+}
+
+void MerkelMain::processUserOption(int userOption) {
+    if (userOption == 1) {
+        printHelp();
+    } else if (userOption == 2) {
+        printMarketStats();
+    } else if (userOption == 3) {
+        enterOffer();
+    } else if (userOption == 4) {
+        enterBid();
+    } else if (userOption == 5) {
+        printWallet();
+    } else if (userOption == 6) {
+        gotoNextTimeFrame();
+    } else {
+        warnInvalid();
+    }
+}
---
+++ C4M1/MerkelMain.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include "OrderBookEntry.h"
+
+using namespace std;
+
+class MerkelMain {
+    public:
+        MerkelMain();
+        /* Call this to start the sim */
+        void init();
+
+    private:
+        void loadOrderBook();
+        void printMenu();
+        void printHelp();
+        void printMarketStats();
+        void enterOffer();
+        void enterBid();
+        void printWallet();
+        void gotoNextTimeFrame();
+        void warnInvalid();
+        int getUserOption();
+        void processUserOption(int userOption);
+
+        vector<OrderBookEntry> orders;
+};

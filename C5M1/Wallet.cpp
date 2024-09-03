d5d9b148690fe909d62c2e5b1000495980dd7adb
jayenliao <jay.liao@fazzfinancial.com>
2024-09-03 17:03:14 +0800

    feat: create Wallet class

---
+++ C5M1/Wallet.cpp
@@ -0,0 +1,14 @@
+#include "Wallet.h"
+
+Wallet::Wallet() {
+
+}
+
+void Wallet::insertCurrency(string type, double amount) {
+
+}
+
+bool Wallet::containsCurrency(string type, double amount) {
+    return false;
+}
+
---
+++ C5M1/Wallet.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Wallet {
+    public:
+        Wallet();
+
+        /** intert currency to the wallet */
+        void insertCurrency(string type, double amount);
+
+        /** check if the wallet contains such much currency or more */
+        bool containsCurrency(string type, double amount);
+
+        /** generate a string representation of the wallet */
+        string toString();
+
+    private:
+        map<string, double> currencies;
+
+};

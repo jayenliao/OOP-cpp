3e4404b74b00134e6951dbc0a77f5755ec01e68f
jayenliao <jay.liao@fazzfinancial.com>
2024-09-03 17:26:06 +0800

    feat: implement Wallet::insertCurrency()

--- C5M1/Wallet.cpp
+++ C5M1/Wallet.cpp
@@ -1,14 +1,29 @@
 #include "Wallet.h"
+using namespace std;
 
 Wallet::Wallet() {
 
 }
 
 void Wallet::insertCurrency(string type, double amount) {
+    double balance;
 
+    if (amount < 0) {
+        throw exception{};
+    }
+    if (currencies.count(type) == 0) {
+        balance = 0;
+    } else {
+        balance = currencies[type];
+    }
+    balance += amount;
+    currencies[type] = balance;
 }
 
 bool Wallet::containsCurrency(string type, double amount) {
     return false;
 }
 
+string Wallet::toString() {
+    return "oink";
+}
